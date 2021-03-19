c9c931183fe25b5fa7d7481b3600d98c7626eaf6
MN-Cool <78140825+MN-Cool@users.noreply.github.com>
2021-03-19 17:41:45 +0000

    TextMeshPro and Input Manager

---
+++ Single Screen Game/2DDemo/InputAction/CallbackContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InputAction
+{
+    public class CallbackContext
+    {
+        internal float ReadValue<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
